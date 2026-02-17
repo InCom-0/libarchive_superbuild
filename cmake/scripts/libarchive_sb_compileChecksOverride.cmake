190e7b692cdd7a764191f842a5457c4bcba60a79
Michal Lurie <Michal.Lurie@gmail.com>
2026-02-17 02:42:38 +0100

    Feat: LZMA working again

--- cmake/scripts/libarchive_sb_compileChecksOverride.cmake
+++ cmake/scripts/libarchive_sb_compileChecksOverride.cmake
@@ -10,14 +10,23 @@ if(MbedTLS_ADDED)
     set(ARCHIVE_CRYPTO_SHA512_MBEDTLS TRUE)
 endif()
 
+
 if(libb2_ADDED)
     set(HAVE_LIBB2 TRUE)
 endif()
+
+if(LibLZMA_ADDED)
+    set(LZMA_API_STATIC ${libarchive_sb_BUILD_STATIC_LIBS})
+    set(WITHOUT_LZMA_API_STATIC OFF)
+    set(HAVE_LZMA_STREAM_ENCODER_MT TRUE)
+endif()
+
 if(zstd_ADDED)
     set(HAVE_LIBZSTD TRUE)
     set(HAVE_ZSTD_compressStream TRUE)
     set(HAVE_ZSTD_minCLevel TRUE)
 endif()
+
 if(zstd_ADDED)
     set(HAVE_PKCS5_PBKDF2_HMAC_SHA1 TRUE)
 endif()
